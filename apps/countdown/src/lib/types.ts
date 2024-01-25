8bb8d2eea65493f994c96709bc057cf30c0bed79
Irina Blagun <irina.blagun@gmail.com>
2024-01-25 14:51:25 +0700

    feat(countdown): change attributes

---
+++ apps/countdown/src/lib/types.ts
@@ -0,0 +1,15 @@
+export enum CountdownAttrNames {
+  Root = 'fb-countdown',
+  Target = 'fb-countdown-target',
+
+  Type = 'fb-countdown-type',
+  Finish = 'fb-countdown-finish',
+}
+
+export enum CountdownType {
+  Weeks = 'weeks',
+  Days = 'days',
+  Hours = 'hours',
+  Minutes = 'minutes',
+  Seconds = 'seconds',
+}
