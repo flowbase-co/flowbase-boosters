ac68f32114336f610b8dc20291abdf5a980aaab7
Irina Blagun <irina.blagun@gmail.com>
2024-12-04 17:52:52 +0400

    chore(gsap-text-highlight): add license

--- apps/gsap-text-highlight/vite.config.js
+++ apps/gsap-text-highlight/vite.config.js
@@ -1,6 +1,14 @@
+import fs from 'fs'
 import { defineConfig } from 'vite'
+import banner from 'vite-plugin-banner'
+
+const bannerContent = fs
+  .readFileSync('./LICENSE', 'utf-8')
+  .match(/```([\s\S]*?)```/)?.[1]
+  .trim()
 
 export default defineConfig({
+  plugins: [banner(bannerContent)],
   server: {
     host: true,
   },
